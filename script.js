1d2f8e38e9e56577b37854d043b2e84e3ea9e06e
Ankitkshw839 <151990328+Ankitkshw839@users.noreply.github.com>
2025-03-04 12:47:47 +0530

    Update script.js
    added dark mode

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-document.getElementById('revealButton').addEventListener('click', function() {
-    document.getElementById('cover').style.display = 'none';
-    document.getElementById('letter').style.display = 'block';
+// Function to toggle dark mode
+document.getElementById('darkModeButton').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
 });
 
+// Function to reveal the invitation letter
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('cover').style.display = 'none';
+    document.getElementById('letter').style.display = 'flex';
+});
+
+// Function to close the invitation letter
 document.getElementById('closeButton').addEventListener('click', function() {
     document.getElementById('letter').style.display = 'none';
     document.getElementById('cover').style.display = 'flex';
